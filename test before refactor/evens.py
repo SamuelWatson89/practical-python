f5a2b3abc8ec480e4865492b36058976ef47e3f2
Samuel <otacon100@hotmail.com>
2019-04-17 21:18:40 +0100

    Single line function

--- test before refactor/evens.py
+++ test before refactor/evens.py
@@ -3,14 +3,8 @@ def is_even(number):
 
 
 def even_number_of_evens(numbers):
-    evens = 0
-    for n in numbers:
-        if is_even(n):
-            evens += 1
-    if evens == 0:
-        return False
-    else:
-        return is_even(evens)
+    evens = sum([1 for n in numbers if is_even(n)])
+    return False if evens == 0 else is_even(evens)
 
 
 assert even_number_of_evens([]) == False, "No numbers"
